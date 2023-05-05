195b7a32f8a21be0e9b91d87a5e23f5e2dceeb29
Tapludeforfait <119884960+Tapludeforfait@users.noreply.github.com>
2023-05-05 14:23:24 +0200

    Delete main directory

--- main/discord_upload.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def upload_to_discord(webhook_url, gif_file):
-    files = {"file": open(gif_file, "rb")}
-    response = requests.post(webhook_url, files=files)
-    response.raise_for_status()
