4969c142ddf9527985abc11444bf567d68c02fa1
Tapludeforfait <regino.rastaquere@gmail.com>
2023-05-05 15:01:56 +0200

    Speechbubble
    Added stuff prob

---
+++ main/main.py
@@ -0,0 +1,71 @@
+import os
+import io
+import base64
+import requests
+from PIL import Image
+from speechbubble import *
+from discord_upload import upload_to_discord
+import pyperclip
+
+WEBHOOK_URL = "https://discord.com/api/webhooks/1103710146888204409/hOsPZM0JLKfeUrdl3p5AY8BvxR2JnVtLo-xLDtL_f-71AMNbhF6G6h4eSu_TdkyJgauj"
+
+def is_url_image(url):
+    try:
+        response = requests.head(url)
+        content_type = response.headers.get('content-type')
+        return content_type and content_type.startswith('image')
+    except:
+        return False
+
+def print_help():
+    print("Prefix 1: Adds a transparent speech bubble on the image (recommended for Images)")
+    print("Prefix 2: Adds a speech bubble above the image (recommended for retards talking)")
+
+def main():
+    image = None
+
+    if pyperclip.is_available() and pyperclip.paste().startswith("data:image"):
+        image = Image.open(io.BytesIO(base64.b64decode(pyperclip.paste().split(',')[1])))
+    else:
+        file = ''
+        if not file:
+            print('Enter a prefix (1 or 2), drag and drop or enter image path/URL ("help" for more info)')
+            file = input().strip('\"')
+        
+        if file.lower() == "help":
+            print_help()
+            return
+
+        prefix, file = file[0], file[1:]
+        if is_url_image(file):
+            image = Image.open(io.BytesIO(requests.get(file).content))
+        elif os.path.exists(file):
+            image = Image.open(file)
+        else:
+            print('Retard, you provided a wrong input, go kys nigger')
+            return
+
+        width, height = image.size
+        ratio = width / height
+        height = image.height
+
+        if prefix == '1':
+            new_image = generate_speechbubble(image)
+        elif prefix == '2':
+            if ratio >= 500/300 or height >= 500:
+                new_image = add_speechbubble(image)
+            else:
+                new_image = add_speechbubble_big_image(image)
+
+        gif_file = 'edited.gif'
+        new_image.save(gif_file)
+
+    try:
+        upload_to_discord(WEBHOOK_URL, gif_file)
+        print("Your retard has been successfully speech-bubbled and uploaded to discord")
+    except requests.exceptions.HTTPError as e:
+        print(f"The gif failed to upload : {e}")
+
+
+if __name__ == '__main__':
+    main()
---
+++ main/readme.md
@@ -0,0 +1,25 @@
+# Simple Speech Bubble Generator for Discord
+
+This small Python project offers the ability to add a speech bubble to any given image or screenshot automatically. After the editing process is complete, the resulting image can be uploaded to the desired Discord webhook.
+
+## How to Use
+
+1. Clone the repository
+2. Make sure you have the latest version of python installed
+3. Install the required libraries by running `requirements.bat`
+4. Set the `WEBHOOK_URL` variable in the `main.py` to your Discord webhook URL
+5. Run the script using `python main.py`
+6. Follow the instructions in the terminal to provide the image and choose the speech bubble style
+
+### Speech Bubble Styles
+
+- **Prefix 1**: Adds a transparent speech bubble on the image (recommended for Images)
+- **Prefix 2**: Adds a speech bubble above the image (recommended for for retards talking)
+
+### Adding Images
+
+You can add an image by providing a file path, drag and dropping the image directly into the terminal or by URL
+
+## Contact
+
+For any questions, feel free to hmu me on Discord: Tap#4852
